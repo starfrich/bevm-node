94be397f326592d75a3cc3f841695dc4f2848a79
Starfish <cryptobethogle@gmail.com>
2024-01-16 14:35:46 +0700

    skip if already has docker

--- script.sh
+++ script.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-# Install Docker on your VPS
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+# Check if Docker is already installed
+if command -v docker &> /dev/null
+then
+    echo "Docker is already installed. Skipping installation."
+else
+    # Download and run the Docker installation script
+    wget https://raw.githubusercontent.com/docker/docker-install/master/docker-install.sh -O - | sudo sh
+fi
 
 # Establish a host mapping path
-cd /var/lib
-mkdir node_bevm_test_storage
+mkdir -p /var/lib/node_bevm_test_storage
 path="/var/lib/node_bevm_test_storage"
 
-# Fetch the Docker image
-sudo docker pull btclayer2/bevm:v0.1.1
+# Fetch the Docker image if not already present
+if sudo docker images | grep -q "btclayer2/bevm:v0.1.1"
+then
+    echo "Docker image btclayer2/bevm:v0.1.1 is already present. Skipping image download."
+else
+    sudo docker pull btclayer2/bevm:v0.1.1
+fi
 
-# Ask user for the node name
+# Run the Docker container
 read -p "Enter your desired node name: " node_name
-
-# Run a Docker container with the provided node name
 telemetry_url="wss://telemetry.bevm.io/submit 0"
 sudo docker run -d -v $path:/root/.local/share/bevm btclayer2/bevm:v0.1.1 bevm "--chain=testnet" "--name=$node_name" "--pruning=archive" --telemetry-url "$telemetry_url"
 
